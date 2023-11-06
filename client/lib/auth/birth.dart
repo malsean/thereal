f72806285d0bf8ac704f89939f87ea5fae37d474
malsean <146160436+malsean@users.noreply.github.com>
2023-11-05 21:00:26 -0800

    Update birth.dart

--- client/lib/auth/birth.dart
+++ client/lib/auth/birth.dart
@@ -1,160 +1,184 @@
-// ignore_for_file: must_be_immutable
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'signup.dart';
+import 'package:rebeal/Auth/signup.dart';
 import '../animation/animation.dart';
 import '../widget/custom/rippleButton.dart';
 
 class BirthPage extends StatefulWidget {
-  String name;
+  final String name;
   final VoidCallback? loginCallback;
-  BirthPage({Key? key, required this.name, this.loginCallback})
-      : super(key: key);
+
+  BirthPage({Key? key, required this.name, this.loginCallback}) : super(key: key);
 
   @override
   _BirthPageState createState() => _BirthPageState();
 }
 
-bool empt = false;
-
 class _BirthPageState extends State<BirthPage> {
-  final _birthController = TextEditingController();
+  int? selectedMonth;
+  int? selectedDay;
+  int? selectedYear;
 
-  @override
-  void initState() {
-    setState(() {
-      _birthController.text.isNotEmpty ? empt = true : empt = false;
-    });
-    super.initState();
+  int calculateAge(int? month, int? day, int? year) {
+    if (month == null || day == null || year == null) return 0;
+
+    final birth = DateTime(year, month, day);
+    final currentDate = DateTime.now();
+    int age = currentDate.year - birth.year;
+    if (birth.month > currentDate.month ||
+        (birth.month == currentDate.month && birth.day > currentDate.day)) {
+      age--;
+    }
+    return age;
   }
 
   @override
   Widget build(BuildContext context) {
+    List<int> dayList = List.generate(
+      selectedMonth == 2
+          ? (selectedYear != null && (selectedYear! % 4 == 0) &&
+                  (selectedYear! % 100 != 0 || selectedYear! % 400 == 0)
+              ? 29
+              : 28)
+          : (selectedMonth == 4 || selectedMonth == 6 || selectedMonth == 9 || selectedMonth == 11 ? 30 : 31),
+      (i) => i + 1,
+    );
+
+    DropdownButton<int> _buildDropdown(String hint, int start, int end, int? currentValue, Function(int?) onChanged) {
+      List<DropdownMenuItem<int>> items = List.generate((end - start + 1), (index) {
+        return DropdownMenuItem<int>(
+          value: start + index,
+          child: Text((start + index).toString()),
+        );
+      });
+
+      return DropdownButton<int>(
+        value: currentValue,
+        items: items,
+        hint: Text(
+          hint,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w400),
+        ),
+        dropdownColor: Colors.black,
+        style: TextStyle(color: Colors.white),
+        onChanged: onChanged,
+      );
+    }
+
     return GestureDetector(
-        onHorizontalDragUpdate: (details) {
-          if (details.delta.dx > 0) {
-            setState(() {
-              Navigator.pop(context);
-            });
-          }
-        },
-        child: Scaffold(
-            appBar: AppBar(
-              elevation: 0,
-              title: Image.asset(
-                "assets/rebeals.png",
-                height: 130,
-              ),
-              backgroundColor: Colors.black,
-            ),
-            extendBodyBehindAppBar: true,
-            backgroundColor: Colors.black,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  height: 130,
-                ),
-                Text(
-                  "Hello ${widget.name}, what is you're birthday ?",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500),
-                  textAlign: TextAlign.center,
-                ),
-                TextField(
-                    textAlign: TextAlign.center,
-                    onChanged: (value) {
-                      setState(() {
-                        _birthController.text.isNotEmpty
-                            ? empt = true
-                            : empt = false;
-                      });
-                      if (_birthController.text.length == 2) {
-                        _birthController.text = _birthController.text + " ";
-                        _birthController.selection = TextSelection.fromPosition(
-                            TextPosition(
-                                offset: _birthController.text.length,
-                                affinity: TextAffinity.upstream));
-                      }
-                      if (_birthController.text.length == 5) {
-                        _birthController.text = _birthController.text + " ";
-                        _birthController.selection = TextSelection.fromPosition(
-                            TextPosition(
-                                offset: _birthController.text.length,
-                                affinity: TextAffinity.upstream));
-                      }
-                      if (_birthController.text.length >= 11) {
-                        _birthController.text = _birthController.text
-                            .substring(0, _birthController.text.length - 1);
-                      }
-                    },
-                    keyboardType: TextInputType.number,
-                    keyboardAppearance: Brightness.dark,
-                    controller: _birthController,
-                    decoration: InputDecoration(
-                        hintText: 'DD MM YYYY',
-                        border: InputBorder.none,
-                        hintStyle: TextStyle(
-                            color: Color.fromARGB(255, 60, 60, 60),
-                            fontSize: 38,
-                            fontWeight: FontWeight.w800)),
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 38,
-                        fontWeight: FontWeight.w800)),
+      onHorizontalDragUpdate: (details) {
+        if (details.delta.dx > 0) {
+          Navigator.pop(context);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Image.asset(
+            "assets/rebeals.png",
+            height: 130,
+          ),
+          backgroundColor: Colors.black,
+        ),
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.black,
+body: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment: CrossAxisAlignment.center,
+  children: [
+    Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        "Hello ${widget.name}, what is your birthday?",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.w500
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+                _buildDropdown('MM', 1, 12, selectedMonth, (val) {
+                  setState(() {
+                    selectedMonth = val;
+                    selectedDay = null; // Reset selected day when month changes
+                  });
+                }),
+                _buildDropdown('DD', 1, dayList.length, selectedDay, (val) {
+                  setState(() {
+                    selectedDay = val;
+                  });
+                }),
+                _buildDropdown(
+                    'YYYY',
+                    DateTime.now().year - 130,
+                    DateTime.now().year - 13,
+                    selectedYear, (val) {
+                  setState(() {
+                    selectedYear = val;
+                  });
+                }),
               ],
             ),
-            bottomSheet: Container(
-                height: 100,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.black,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RippleButton(
-                        splashColor: Colors.transparent,
-                        child: Container(
-                            height: 70,
-                            width: MediaQuery.of(context).size.width - 40,
-                            decoration: BoxDecoration(
-                              color: empt || _birthController.text.isNotEmpty
-                                  ? Colors.white
-                                  : Color.fromARGB(255, 61, 61, 61),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Center(
-                                child: Text(
-                              "Continue",
-                              style: TextStyle(
-                                  fontFamily: "icons.ttf",
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w800),
-                            ))),
-                        onPressed: () {
-                          if (_birthController.text.isNotEmpty) {
-                            HapticFeedback.heavyImpact();
-                            Navigator.push(
-                              context,
-                              AwesomePageRoute(
-                                transitionDuration: Duration(milliseconds: 600),
-                                exitPage: widget,
-                                enterPage: Signup(
-                                    name: widget.name,
-                                    birth: _birthController.text),
-                                transition: ZoomOutSlideTransition(),
-                              ),
-                            );
-                          }
-                        },
-                      )
-                    ],
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: RippleButton(
+                splashColor: Colors.transparent,
+                child: Container(
+                  height: 70,
+                  width: MediaQuery.of(context).size.width - 40,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                ))));
+                  child: Center(
+                      child: Text(
+                    "Continue",
+                    style: TextStyle(
+                        fontFamily: "icons.ttf",
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800),
+                  )),
+                ),
+                onPressed: () {
+                  if (selectedMonth != null && selectedDay != null && selectedYear != null) {
+                    final age = calculateAge(selectedMonth, selectedDay, selectedYear);
+                    if (age >= 13 && age <= 130) {
+                      HapticFeedback.heavyImpact();
+                      Navigator.push(
+                        context,
+                        AwesomePageRoute(
+                          transitionDuration: Duration(milliseconds: 600),
+                          exitPage: widget,
+                          enterPage: Signup(
+                            name: widget.name,
+                            birth: "$selectedMonth $selectedDay $selectedYear",
+                          ),
+                          transition: ZoomOutSlideTransition(),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Age must be between 13 and 130'),
+                        ),
+                      );
+                    }
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
